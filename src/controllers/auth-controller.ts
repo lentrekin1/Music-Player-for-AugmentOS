6aaaee70a8497f3c34dc7416123b2dd53db31441
Gikaeh <gbabynicosia@gmail.com>
2025-04-09 01:48:22 -0700

    Fixed tpa config sending to cloud

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -3,6 +3,7 @@ import {spotifyService} from '../services/spotify-service';
 import {activeSessions} from '../server';
 import {displayCurrentlyPlaying} from '../handlers/session-handler';
 import logger from '../utils/logger'
+import path from 'path'
 
 const router = Router();
 
@@ -59,5 +60,23 @@ router.get('/callback', async (req: Request, res: Response) => {
     
 //   }
 // })
+const settingsSchemaPathPublic = path.join(__dirname, '../public', 'tpa_config.json');
+router.get('/tpa_config.json', async (req: Request, res: Response) => {
+  try {
+    res.sendFile(settingsSchemaPathPublic)
+  } catch (error) {
+    logger.error('Authentication error:', {
+      res: res,
+      req: req,
+      error: {
+        message: error.message,
+        stack: error.stack,
+        responseStatus: error.response?.status,
+        responseBody: error.response?.data 
+      }
+    });
+    res.send('Error retrieving tpa_config.json')
+  }
+})
 
 export const authRoutes = router;
